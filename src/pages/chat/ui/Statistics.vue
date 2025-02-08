753f1313143eb46606c6f30f89f1532e06672ffd
Dmitrii Baranov <me@dmbaranov.io>
2025-02-08 21:08:46 +0200

    Added loading and error state for Statistics view

--- src/pages/chat/ui/Statistics.vue
+++ src/pages/chat/ui/Statistics.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import { TopUsers, CommandsTimeline, TopCommands, UsersTimeline } from '@/features/statistics';
-import { ref } from 'vue';
+import { GeneralError } from '@/widgets/Errors';
+import { useGetChatStatistics } from '@/entities/statistics';
+import { useGetChatId } from '@/shared/lib';
+import { useGetChatUsers } from '@/entities/user';
+
+const chatId = useGetChatId();
+const { isLoading: statisticsLoading, error: statisticsError } = useGetChatStatistics(chatId);
+const { isLoading: usersLoading, error: usersError } = useGetChatUsers(chatId);
 
 const selectedTab = ref('Top users');
+const dataError = computed(() => statisticsError.value ?? usersError.value);
 
 const charts = [
   {
@@ -25,16 +34,22 @@ const charts = [
 </script>
 
 <template>
-  <VContainer class="w-75">
-    <VTabs v-model="selectedTab" align-tabs="center">
-      <VTab v-for="chart in charts" :key="chart.name" :value="chart.name">{{ chart.name }}</VTab>
-    </VTabs>
-    <VTabsWindow v-model="selectedTab">
-      <VTabsWindowItem v-for="chart in charts" :key="chart.name" :value="chart.name">
-        <VRow class="my-8 mx-2">
-          <component :is="chart.component" />
-        </VRow>
-      </VTabsWindowItem>
-    </VTabsWindow>
+  <GeneralError :error="dataError" />
+  <VContainer v-if="!dataError" class="w-75 h-100">
+    <VRow v-if="statisticsLoading ?? usersLoading" class="h-100 justify-center align-center">
+      <VProgressCircular v-if="true" color="primary" indeterminate />
+    </VRow>
+    <template v-else>
+      <VTabs v-model="selectedTab" align-tabs="center">
+        <VTab v-for="chart in charts" :key="chart.name" :value="chart.name">{{ chart.name }}</VTab>
+      </VTabs>
+      <VTabsWindow v-model="selectedTab">
+        <VTabsWindowItem v-for="chart in charts" :key="chart.name" :value="chart.name">
+          <VRow class="my-8 mx-2">
+            <component :is="chart.component" />
+          </VRow>
+        </VTabsWindowItem>
+      </VTabsWindow>
+    </template>
   </VContainer>
 </template>
