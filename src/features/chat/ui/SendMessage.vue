567ce7682c5e0517aa27e030321ba4026abe9761
Dmitrii Baranov <me@dmbaranov.io>
2025-02-08 20:18:46 +0200

    Fixed incorrect notification for messages sending and added loading state

--- src/features/chat/ui/SendMessage.vue
+++ src/features/chat/ui/SendMessage.vue
@@ -6,24 +6,27 @@ import { useGetChatPlatform } from '../lib/useGetChatPlatform.ts';
 
 const chatId = useGetChatId();
 const { platform } = useGetChatPlatform(chatId);
-const { mutateAsync, error } = useSendMessageToChat(platform, chatId);
+const { mutateAsync, isPending, error } = useSendMessageToChat(platform, chatId);
 const messageText = ref('');
 const snackbarShown = ref(false);
 
 function sendMessage() {
-  mutateAsync({ chatId, message: messageText.value });
-
-  messageText.value = '';
-  snackbarShown.value = true;
+  mutateAsync({ chatId, message: messageText.value })
+    .then(() => {
+      messageText.value = '';
+    })
+    .finally(() => {
+      snackbarShown.value = true;
+    });
 }
 </script>
 
 <template>
   <VForm class="w-100 d-flex flex-wrap" @submit.prevent="sendMessage">
     <VTextarea v-model="messageText" class="w-100" label="Message" variant="outlined" />
-    <VBtn class="ml-auto" type="submit" color="blue">Send</VBtn>
+    <VBtn class="ml-auto" type="submit" color="blue" :loading="isPending">Send</VBtn>
   </VForm>
   <VSnackbar v-model="snackbarShown" :timeout="2000">
-    <div>{{ error ? `Message sending failed: ${error}` : 'Message sent!' }}</div>
+    <div>{{ error ? `Message sending failed: ${error.message}` : 'Message sent!' }}</div>
   </VSnackbar>
 </template>
